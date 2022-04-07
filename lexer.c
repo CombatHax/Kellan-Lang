f00ca893e8907c5dec04bb3eed6cbac133c02877
CombatHax <92267016+CombatHax@users.noreply.github.com>
2022-04-07 15:26:58 -0400

    Add files via upload

--- lexer.c
+++ lexer.c
@@ -4,24 +4,23 @@
 
 typedef struct {
     int* actions;
-    int* locs;
-} actions;
-typedef struct {
-    int* actions;
     char* name;
 } function;
+typedef struct {
+    char* name;
+    void* value;
+    char t;
+} variable;
 
 char* read(FILE* f) {
+    variable* vars = malloc(1);
     char* acts = malloc(1);
     char c = fgetc(f);
     unsigned long row = 0;
-    short quotes = 0;
     int spaces = 0;
     while(c != EOF) {
         row++;
-        if(c == '"') 
-            quotes = !(quotes % 2) ? 1 : 0;
-        if(c == ' ' && !quotes) {
+        if(c == ' ') {
             if(spaces == 3) {
                 spaces = 0;
                 c = '\t';
@@ -35,19 +34,25 @@ char* read(FILE* f) {
         acts[row - 1] = c;
         c = fgetc(f);
     }
+    fclose(f);
     char* tok = strtok(acts, "\n");
     char* mem = malloc(4);
     for(int i = 0; tok != NULL; i++) {
-        memcpy(mem, tok, 4);
-        if(!(strcmp(mem, "prt "))) {
-            if(sizeof(tok) < 5) {
-                printf("Problemo");
-                tok = strtok(NULL, "\n");
-                continue;
-            }
-            char* val = malloc(sizeof(tok) - 4);
-            strcpy(val, tok + 4);
+        memcpy(mem, tok, 6);
+        if(!(strcmp(mem, "write "))) {
+            char* val = malloc(sizeof(tok) - 6);
+            strcpy(val, tok + 6);
             printf("%s\n", val);
+            free(val);
+        }
+        else if(strchr(tok, '=') != NULL) {
+            char* c = strchr(tok, '=');
+            unsigned long in = (unsigned long)(c - tok);
+            char* name = strtok(tok, "=");
+            if(name[sizeof(name) - 2] == ' ') {
+                name[sizeof(name) - 2] = '\0';
+            }
+            printf("%s\n", name);
         }
         tok = strtok(NULL, "\n");
     }
