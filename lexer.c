e5942ba66004be7311c2230665ce1676e89feb0f
CombatHax <92267016+CombatHax@users.noreply.github.com>
2022-04-07 11:31:44 -0400

    Add files via upload

--- lexer.c
+++ lexer.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    int* actions;
+    int* locs;
+} actions;
+typedef struct {
+    int* actions;
+    char* name;
+} function;
+
 char* read(FILE* f) {
     char* acts = malloc(1);
-    char** act = malloc(1);
     char c = fgetc(f);
     unsigned long row = 0;
     short quotes = 0;
@@ -15,7 +23,6 @@ char* read(FILE* f) {
             quotes = !(quotes % 2) ? 1 : 0;
         if(c == ' ' && !quotes) {
             if(spaces == 3) {
-                printf("Tab");
                 spaces = 0;
                 c = '\t';
                 acts = realloc(acts, row);
@@ -29,12 +36,19 @@ char* read(FILE* f) {
         c = fgetc(f);
     }
     char* tok = strtok(acts, "\n");
-    long s = 0;
+    char* mem = malloc(4);
     for(int i = 0; tok != NULL; i++) {
-        s = sizeof(tok);
-        act = realloc(act, sizeof(act) + s);
-        act[i] = realloc(act[i], s);
-        strcpy(act[i], tok);
+        memcpy(mem, tok, 4);
+        if(!(strcmp(mem, "prt "))) {
+            if(sizeof(tok) < 5) {
+                printf("Problemo");
+                tok = strtok(NULL, "\n");
+                continue;
+            }
+            char* val = malloc(sizeof(tok) - 4);
+            strcpy(val, tok + 4);
+            printf("%s\n", val);
+        }
         tok = strtok(NULL, "\n");
     }
     return acts;
--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #include "lexer.c"
 
 int main(int args, char* argv[]) {
-    FILE* file = fopen("main.kln", "r");
-    if(file == NULL) {
-        printf("Invalid File Name\n");
+    FILE* file = fopen(argv[1], "r");
+    if(args < 2 || file == NULL) {
+        printf("Invalid file name\n");
         return 1;
     }
     char* text = read(file);
